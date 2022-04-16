4c5a4c9c810470d65747af68ffcd7f8d77b27f0b
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-04-16 17:44:14 +0200

    Add style

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './styles/constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nice books!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: colors.background,
+    color: colors.text,
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });

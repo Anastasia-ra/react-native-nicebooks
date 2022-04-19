41eba926705585fda32787e81e0c0c4259585195
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-04-19 09:02:02 +0200

    Style homepage

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../styles/constants';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Welcome to Nicebooks!</Text>
+    <View style={styles.container}>
+      <Text style={[styles.text, { fontSize: 32 }]}>Book shelf</Text>
+      <Text style={styles.text}>Categories</Text>
+      <Text style={styles.text}>Latest additions</Text>
+      <Text style={styles.text}>See more</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'serif',
+    color: colors.textColor,
+    fontSize: 24,
+  },
+});

14f834d418eff1f89e3c21c7774c811328c01760
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-04-19 14:01:11 +0200

    Add random quotes generator

--- screens/RandomQuotes.tsx
+++ screens/RandomQuotes.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from '../styles/constants';
+import { Button, Input } from 'react-native-elements';
 
 export default function RandomQuotes({ navigation }) {
+  const [quote, setQuote] = useState<string>('Get ready to be inspired');
+
+  async function getRandomQuote() {
+    const apiUrl = 'https://zenquotes.io/api/random/';
+
+    const response = await fetch(apiUrl);
+    const quoteResponse = await response.json();
+    console.log(quoteResponse);
+    const quote2 = quoteResponse[0].q;
+    console.log(quote2, typeof quote2);
+    setQuote(quote2);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, { fontSize: 32 }]}>
-        Random quotes generator
-      </Text>
-      <Text style={styles.text}>Categories</Text>
-      <Text style={styles.text}>Latest additions</Text>
-      <Text style={styles.text}>See more</Text>
+      <Text style={styles.text}>{quote}</Text>
+      <Button
+        title="Display random quote"
+        // buttonStyle={styles.button}
+        // containerStyle={styles.buttonContainer}
+        onPress={async () => await getRandomQuote()}
+      />
     </View>
   );
 }

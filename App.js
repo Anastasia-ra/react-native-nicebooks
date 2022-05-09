e63f04f371c65b0590865923cd3a6ef7539e1a4d
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-05-09 09:23:53 +0200

    Add Apollo Client

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 import 'react-native-gesture-handler';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
@@ -8,20 +14,48 @@ import Register from './screens/Register';
 
 const Stack = createStackNavigator();
 
+const cache = new InMemoryCache();
+const link = createHttpLink({
+  uri: 'https://nicebooks-backend.herokuapp.com/', // '/graphql',
+  credentials: 'same-origin', // 'include',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+      errorPolicy: 'ignore',
+      notifyOnNetworkStatusChange: true,
+    },
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all',
+    },
+    mutate: {
+      errorPolicy: 'ignore',
+      fetchPolicy: 'network-only',
+    },
+  },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="RandomQuotes" component={RandomQuotes} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="RandomQuotes" component={RandomQuotes} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
